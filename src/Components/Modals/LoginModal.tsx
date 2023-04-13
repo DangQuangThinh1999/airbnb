aa73ff0da38ab53dfdb5ede499a31d02a2269504
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-04-13 14:40:12 +0700

    setup prisma and mongodb

---
+++ src/Components/Modals/LoginModal.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import useRegisterModal from "../hooks/useRegisterModal";
+import axios from "axios";
+import Modal from "./Modal";
+import BodyContent from "./BodyContent";
+import FooterContent from "./FooterContent";
+const LoginModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error("Something went wrong");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={
+        <BodyContent
+          errors={errors}
+          isLoading={isLoading}
+          register={register}
+        />
+      }
+      footer={<FooterContent />}
+    />
+  );
+};
+
+export default LoginModal;
