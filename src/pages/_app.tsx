9e806934831365752eae0b057007c2c44c0b3c6a
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-04-13 14:27:41 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import ClientOnly from "@/Components/ClientOnly";
-<<<<<<< Updated upstream
-import Modal from "@/Components/Modals/Modal";
-=======
 import LoginModal from "@/Components/Modals/LoginModal";
 import RegisterModal from "@/Components/Modals/RegisterModal";
->>>>>>> Stashed changes
 import Navbar from "@/Components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -16,13 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
       <ClientOnly>
-<<<<<<< Updated upstream
-        <Modal />
-=======
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
->>>>>>> Stashed changes
         <Navbar />
       </ClientOnly>
       <Component {...pageProps} />
