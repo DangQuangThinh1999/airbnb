e5c76ebc245b5ac3f825ad01ae3b660e302e67bb
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-05-17 14:05:02 +0700

    fix: fix 02

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.jsx",
-    "./components/**/*.jsx",
   ],
   theme: {
     extend: {
